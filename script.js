363cfb1fbca48a779474beeea11588731f202dff
Nao <ktimeabarbara@gmail.com>
2025-05-09 20:29:23 +0200

    javitva

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-window.addEventListener("load", function () {
+ window.addEventListener("load", function () {
     elemekElerese1();
     elemekElerese2();
     elemekElerese3();
-    elemekElerese4();   
+    elemekElerese4();
+    esemenyKezeles1();
+    esemenyKezeles2();
+    esemenyKezeles4();
 });
 
 function elemekElerese1() {
@@ -31,12 +34,46 @@ function elemekElerese4() {
     elem4.classList.add("formazott")
 }
 function esemenyKezeles1() {
-    let elem4 = document.querySelectorAll(".lista")[0];
+    let elem4 = document.querySelector(".lista");
     elem4.addEventListener("click", fveny);
+    function fveny() {
+        const kattintasutanElem = document.querySelector(".kattintasutan"); /* ahova írni akarok */
+        kattintasutanElem.innerHTML=elem4.innerHTML;
+        console.log(kattintasutanElem.innerHTML)
+    }
 }
-function fveny() {
-    let elem4 = document.querySelectorAll(".lista")[0]; /* aminek a tartalma kell */
-    const kattintasutanElem = document.querySelectorAll(".kattintasutan")[0]; /* ahova írni akarok */
-    kattintasutanElem.appendChild(elem4);
+function esemenyKezeles2() {
+    let elem5 = document.querySelector(".feladat");
+    elem5.innerHTML = "<button>OK</button>";
+    let gomb = document.querySelector(".feladat button");
+    gomb.addEventListener("click", fveny);
+    function fveny() {
+        elem5.innerHTML += "<div><img src='https://us-tuna-sounds-images.voicemod.net/ec521c4c-c05d-402e-b90f-eb5b2b3dc595-1679647518156.png' id='kep'></div>"
+        esemenyKezeles3();
+    }
+    function esemenyKezeles3() {
+        let elem = document.querySelector("#kep");
+        elem.addEventListener("mouseenter", fveny);
+        elem.addEventListener("mouseleave", fveny2);
+        function fveny() {
+            elem.classList.add("nagyit");
+            console.log("nagy kep")
+        }
+        function fveny2() {
+            elem.classList.remove("nagyit");
+            console.log("normalis")
+        }
+    }
+}
+function esemenyKezeles4() {
+    let tarolo = document.querySelectorAll(".tarolo div");
+    let eredmeny = document.querySelector(".eredmeny");
+    for (let i = 0; i < tarolo.length; i++) {
+        tarolo[i].addEventListener("click", fveny);
+        function fveny() {
+            eredmeny.innerHTML += tarolo[i].innerHTML;
+        }
+    }
+
 }
 
